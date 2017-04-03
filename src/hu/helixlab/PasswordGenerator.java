483436c395fa08413518717f8e74f265ddf6f663
wolfika222 <farkas0528@gmail.com>
2017-04-03 13:44:32 +0200

    #1

--- src/hu/helixlab/PasswordGenerator.java
+++ src/hu/helixlab/PasswordGenerator.java
@@ -11,11 +11,10 @@ public class PasswordGenerator {
     public static String generatePassword(int length) {
 
         String alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!%?";
-        int number = alpha.length();
         String result = new String();
         Random password = new Random();
         for (int i = 0; i < length; i++){
-            result = result + alpha.charAt(password.nextInt(number));
+            result += (password.nextInt(alpha.length()));
         }
             return result;
     }
