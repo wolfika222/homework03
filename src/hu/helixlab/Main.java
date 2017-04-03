d81aefd53df5abdea320f18d42ab1fda139c2dc2
wolfika222 <farkas0528@gmail.com>
2017-04-03 08:59:23 +0200

    #1

---
+++ src/hu/helixlab/Main.java
@@ -0,0 +1,22 @@
+package hu.helixlab;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*
+        User user1 = new User("Jóska");
+        System.out.println(user1.toString());
+
+
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println((new User("Gábor")).toString());
+        }
+     */
+        System.out.println(PasswordGenerator.generatePassword(10));
+    }
+}
---
+++ src/hu/helixlab/PasswordGenerator.java
@@ -0,0 +1,21 @@
+package hu.helixlab;
+
+import java.util.Random;
+
+/**
+ * Created by Hp_Workplace on 2017.04.03..
+ */
+public class PasswordGenerator {
+
+
+    public static String generatePassword(int length) {
+        String alpha = ("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!%?");
+        int number = alpha.length();
+        String result = new String();
+        Random password = new Random();
+        for (int i = 0; i < length; i++){
+            result = result + alpha.charAt(password.nextInt(number));
+        }
+            return result;
+    }
+}
